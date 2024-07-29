185f80f5a75a07b37decf3ff61e43ba6a77112a2
Karina Bianek <bianekkarina@gmail.com>
2024-07-29 15:59:08 -0300

    2, 4, 7, 8, 10, 12

--- 2-saudarusuarios.js
+++ 2-saudarusuarios.js
@@ -3,8 +3,17 @@ Crie uma função chamada greetUsers que aceita um array de nomes e
 exibe uma saudação para cada nome no console.
 
 Programa desenvolvido por:
- - Aluno 1
- - Aluno 2
- - Aluno 3
- - Aluno 4
+ - Alexandre
+ - José
+ - Kailane
+ - Karina
 */
+
+function greetUsers (vetor) {
+    vetor.forEach(nome => {
+         console.log("Parabéns " + nome)
+    });
+
+}
+
+greetUsers(["kailane", "karina"])
