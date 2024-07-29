1bf93300825ba5d500e267d54a2477402bdc7d84
Karina Bianek <bianekkarina@gmail.com>
2024-07-29 16:07:13 -0300

    2, 4, 7, 8, 10, 12

--- 8-triangulos.js
+++ 8-triangulos.js
@@ -8,8 +8,21 @@ na primeira linha e adicionando um asterisco a cada linha subsequente.
 Você pode usar a função .repeat().
 
 Programa desenvolvido por:
- - Aluno 1
- - Aluno 2
- - Aluno 3
- - Aluno 4
+ - Alexandre
+ - José
+ - Kailane
+ - Karina
 */
+
+function drawTriangle(n) {
+    for (let i = 0; i <= n; i++) {
+        console.log('*'.repeat(i))
+    }
+}
+
+//exemplos de uso
+drawTriangle(5)
+drawTriangle(3)
+drawTriangle(15)
+
+//site de pesquisa: https://www.freecodecamp.org/portuguese/news/tres-maneiras-de-repetir-uma-string-em-javascript/
